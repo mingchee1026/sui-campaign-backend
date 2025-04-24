e03b3a9de4d6263d5c1d39dc97104e71949a386a
cheeming <dragon.steal75@gmail.com>
2025-04-24 18:18:15 +0900

    feat: update profit function

--- src/index.ts
+++ src/index.ts
@@ -81,8 +81,8 @@ process.on("SIGTERM", onCloseSignal);
 ////////// 0.0₅44226/$0.0₅72101
 
 const calculatePNL = () => {
-  const purchasePrice = 0.000008; // 0.0000183;
-  const finalPrice = 0.000014; //0.0000239;
+  const purchasePrice = 0.0000053; // 0.0000183;
+  const finalPrice = 0.0000041; //0.0000239;
 
   const pnlPercentage = ((finalPrice - purchasePrice) / purchasePrice) * 100;
 
